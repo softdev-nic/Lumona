414a960c53732d9977c8ab1fd55cf55eee95544f
softdev-nic <softtechon.inc@gmail.com>
2026-05-10 15:59:41 +0530

    small changes

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ require('./db');
 require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-const port = 3000;
+
 app.use(cors());
 app.use(express.json());
 
