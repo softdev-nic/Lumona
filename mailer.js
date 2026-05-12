2d6b7fade972f94107d9f9f8ebb9f560346e34b4
softdev-nic <softtechon.inc@gmail.com>
2026-05-12 20:43:45 +0530

    mailer bug fix

--- mailer.js
+++ mailer.js
@@ -1,8 +1,10 @@
  const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
+ const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD
