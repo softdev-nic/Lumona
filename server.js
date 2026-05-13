47fe9df793564cf48d9e2a74b79de179de2cd594
softdev-nic <softtechon.inc@gmail.com>
2026-05-13 13:57:06 +0530

    CORS origin update

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use(cors());
+app.use(cors({
+    origin:[
+        'https://lumona.site',
+        'https://www.lumona.site'
+        ''
+    ]
+}));
 app.use(express.json());
 
 app.post('/api/register', createUser);
