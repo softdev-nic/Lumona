6c2cc2363c9de4bebf630a8f1621370dc4470c44
softdev-nic <softtechon.inc@gmail.com>
2026-05-25 17:53:15 +0530

    invitation modification

--- Modals/Controllers/TeamControllers/AddMember.js
+++ Modals/Controllers/TeamControllers/AddMember.js
@@ -14,7 +14,7 @@ const addMember = async (req, res) => {
             CreatedBy: req.user.user.id
         });
     
-        const InvitationLink = `https://www.teams.lumona.site/accept-invitation/${team.invitationToken}`;
+        const InvitationLink = `https://teams.lumona.site/accept-invitation/${team.invitationToken}`;
         await sendEmail(
             email,
             "Team Invitation",
